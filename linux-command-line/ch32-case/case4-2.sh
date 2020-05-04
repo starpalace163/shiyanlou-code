46fc866203b7a5c66928c67aac70e054c4c22551
starpalace163 <starpalace@163>
2020-05-04 15:30:01 +0800

    case

---
+++ linux-command-line/ch32-case/case4-2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# case4-1: test a character
+read -n 1 -p "Type a character -> "
+echo
+case $REPLY in
+    [[:upper:]])    echo "'$REPLY' is upper case." ;;&
+    [[:lower:]])    echo "'$REPLY' is lower case." ;;&
+    [[:alpha:]])    echo "'$REPLY' is alphabetic." ;;&
+    [[:digit:]])    echo "'$REPLY' is a digit." ;;&
+    [[:graph:]])    echo "'$REPLY' is a visible character." ;;&
+    [[:punct:]])    echo "'$REPLY' is a punctuation symbol." ;;&
+    [[:space:]])    echo "'$REPLY' is a whiteapce character." ;;&
+    [[:xdigit:]])   echo "'$REPLY' is a hexdecimal digit." ;;&
+esac
