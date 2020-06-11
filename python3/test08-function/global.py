bb65abfdbeb550c7ffe51116625ea259792309ea
starpalace163 <starpalace@163>
2020-06-11 16:58:13 +0800

    gloabl.py

---
+++ python3/test08-function/global.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+a = 9
+def change():
+    global a
+    print(a)
+    a = 100
+print("Before the function call ", a)
+print("Inside change function", end=' ')
+change()
+print("After the function call ", a)
+
