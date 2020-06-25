625d73be690d5192008ac5ab8050e49ef24dbbb9
starpalace163 <starpalace@163>
2020-06-25 22:52:48 +0800

    factorial_test.py

---
+++ python3/test17-test/factorial_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import unittest
+from factorial import fact
+
+class TestFactorial(unittest.TestCase):
+    def test_fact(self):
+        res = fact(5)
+        self.assertEqual(res, 121)
+
+if __name__ == '__main__':
+    unittest.main()
+
