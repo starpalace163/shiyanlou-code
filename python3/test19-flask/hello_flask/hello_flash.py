4d14817506439aa6e269b0b4b0c11d1898f3642e
starpalace163 <starpalace@163>
2020-06-29 17:05:58 +0800

    hello_flask

---
+++ python3/test19-flask/hello_flask/hello_flash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import flask
+
+# Create the application.
+APP = flask.Flask(__name__)
+
+@APP.route('/')
+def index():
+    """
+    """
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    APP.debug = True
+    APP.run()
+
