9aee84e1f756311f06b9720e4e3cabede5b4adb3
starpalace163 <starpalace@163>
2020-06-29 12:03:49 +0800

    setup.py

---
+++ python3/test18-project/factorial/build/lib/myfact/fact.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"myfact module"
+
+def factorial(nm):
+    if num >= 0:
+        if num == 0:
+            return 1
+        return num * factorial(num-1)
+    else:
+        return -1
+
