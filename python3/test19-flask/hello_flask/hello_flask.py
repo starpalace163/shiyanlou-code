393024cd095817f4a029ede280e6a982d90b6183
starpalace163 <starpalace@163>
2020-06-30 10:45:54 +0800

    test19-flask

--- python3/test19-flask/hello_flask/hello_flask.py
+++ python3/test19-flask/hello_flask/hello_flask.py
@@ -11,6 +11,12 @@ def index():
     """
     return flask.render_template('index.html')
 
+@APP.route('/hello/<name>/')
+def hello(name):
+    """
+    """
+    return flask.render_template('hello.html', name=name)
+
 if __name__ == '__main__':
     APP.debug = True
     APP.run()
